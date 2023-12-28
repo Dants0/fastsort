b81c9cab2fa8eac37ef550de4617b3ec56ea8451
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2023-12-28 11:29:36 -0300

    (add): adicionei o modelo mergesorte para poder ajudar

--- lib/fastsort.rb
+++ lib/fastsort.rb
@@ -40,4 +40,45 @@ module Fastsort
       array[idx_a], array[idx_b] = array[idx_b], array[idx_a]
     end
   end
+
+  class Mergesort
+    attr_accessor :array
+
+    def initialize(array)
+      @array = array
+    end
+
+    def sort_array
+      return array if array.length <= 1
+
+      middle = array.length / 2
+      left_half = array[0...middle]
+      right_half = array[middle..-1]
+
+      left_half = Mergesort.new(left_half).sort_array
+      right_half = Mergesort.new(right_half).sort_array
+
+      merge(left_half, right_half)
+    end
+
+    def merge(left, right)
+      result = []
+      left_idx = right_idx = 0
+
+      while left_idx < left.length && right_idx < right.length
+        if left[left_idx] < right[right_idx]
+          result << left[left_idx]
+          left_idx += 1
+        else
+          result << right[right_idx]
+          right_idx += 1
+        end
+      end
+
+      result.concat(left[left_idx..-1]) if left_idx < left.length
+      result.concat(right[right_idx..-1]) if right_idx < right.length
+
+      result
+    end
+  end
 end
